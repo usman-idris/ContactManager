3def21cdf84e2ba071b85a6be837059955a6c1b5
Usman Idris Kwaru <kwaru91@gmail.com>
2021-12-20 13:24:34 +0100

    Add project files.

---
+++ ContactManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ContactManager.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contact { get; set; }
+    }
+}
